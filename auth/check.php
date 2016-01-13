e8e3303e3f51889f61a8038e9e8018fc569c4206
russlanpsu <ruslanpsu@yandex.ru>
2016-01-13 20:15:36 +0500

    добавление

--- auth/check.php
+++ auth/check.php
@@ -1,27 +1,29 @@
 <?php
 # Соединямся с БД 
-mysql_connect("localhost", "myhost", "myhost"); 
-mysql_select_db("testtable"); 
+include("settings.php");
+$mysqli = new mysqli($hostName, $userName, $password, $dbName);
 
 if (isset($_COOKIE['id']) and isset($_COOKIE['hash'])) 
-{ 
-$query = mysql_query("SELECT *,INET_NTOA(user_ip) FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1"); 
-$userdata = mysql_fetch_assoc($query); 
+{
 
-if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
-or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR']) and ($userdata['user_ip'] !== "0"))) 
-{ 
-setcookie("id", "", time() - 3600*24*30*12, "/"); 
-setcookie("hash", "", time() - 3600*24*30*12, "/"); 
-print "Хм, что-то не получилось"; 
-} 
-else 
-{ 
-print "Привет, ".$userdata['user_login'].". Всё работает!"; 
-} 
-} 
-else 
-{ 
-print "Включите куки"; 
+	$sql = "SELECT *, INET_NTOA(user_ip) FROM auth_users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1";
+	$result = $mysqli->query($sql);
+	$userdata = mysqli_fetch_assoc($result);
+
+	if (
+		($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
+	//	or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR']) and ($userdata['user_ip'] !== "0"))
+		)
+	{
+		setcookie("id", "", time() - 3600*24*30*12, "/");
+		setcookie("hash", "", time() - 3600*24*30*12, "/");
+		print "Хм, что-то не получилось";
+	}
+	else {
+		print "Привет, ".$userdata['user_login'].". Всё работает!";
+	}
+}
+else{
+	print "Включите куки";
 } 
 ?> 
--- auth/register.php
+++ auth/register.php
@@ -16,21 +16,20 @@ if(isset($_POST['submit']))
 $err = array(); 
 
 # проверям логин 
-if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login'])) 
-{ 
-$err[] = "Логин может состоять только из букв английского алфавита и цифр"; 
+if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login'])) {
+ $err[] = "Логин может состоять только из букв английского алфавита и цифр";
 } 
 
-if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30) 
-{ 
-$err[] = "Логин должен быть не меньше 3-х символов и не больше 30"; 
+if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30) {
+ $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
 } 
 
-# проверяем, не сущестует ли пользователя с таким именем 
-$result = $mysqli->query("SELECT COUNT(user_id) AS CNT FROM users WHERE user_login='".$mysqli->real_escape_string($_POST['login'])."'"); 
-
+# проверяем, не сущестует ли пользователя с таким именем
+$sql = "SELECT COUNT(user_id) AS CNT FROM auth_users WHERE user_login='".$mysqli->real_escape_string($_POST['login'])."'";
+$result = $mysqli->query($sql);
+$data = mysqli_fetch_assoc($result);
 //if(mysql_result($query, 0) > 0) 
-if (mysqli_fetch_assoc($result)["CNT"]){ 
+if ($data['CNT']){
 	$err[] = "Пользователь с таким логином уже существует в базе данных"; 
 } 
 
@@ -38,25 +37,24 @@ if (mysqli_fetch_assoc($result)["CNT"]){
 if(count($err) == 0) 
 { 
 
-$login = $_POST['login']; 
+ $login = $_POST['login'];
 
-# Убераем лишние пробелы и делаем двойное шифрование 
-$password = md5(md5(trim($_POST['password']))); 
+ # Убераем лишние пробелы и делаем двойное шифрование
+ $password = md5(md5(trim($_POST['password'])));
 
-//mysql_query("INSERT INTO users SET user_login='".$login."', user_password='".$password."'"); 
-$mysqli->query("INSERT INTO users SET user_login='".$login."', user_password='".$password."'"); 
-header("Location: login.php"); exit(); 
-} 
-else 
-{ 
-print "При регистрации произошли следующие ошибки:
-"; 
-foreach($err AS $error) 
-{ 
-print $error."
-"; 
-} 
-} 
+ //mysql_query("INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+ $sql = "INSERT INTO auth_users SET user_login='".$login."', user_password='".$password."'";
+ $mysqli->query($sql);
+ header("Location: login.php"); exit();
+ }
+ else
+ {
+  print "При регистрации произошли следующие ошибки:
+  ";
+  foreach($err AS $error) {
+    print $error."\n";
+  }
+ }
 } 
 ?> 
 
